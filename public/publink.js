9013df801a024a52a40f7c68b1cf56e886af4460
Brandon Lum <blum>
2019-10-16 10:41:24 -0400

    Changed publink.js file for heroku

--- public/publink.js
+++ public/publink.js
@@ -1,4 +1,4 @@
-const serverUrl = 'http://localhost:3000';
+const serverUrl = 'https://sleepy-cove-53573.herokuapp.com';
 
 const contentElement = document.getElementById('content');
 //Create and return a DOM element showing a link
